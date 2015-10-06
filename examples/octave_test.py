7dd5d2e2bb1889340497c25329bc929e2add9b72
Evan Palmer <evjpalmer@gmail.com>
2015-10-06 14:29:22 -0700

    more octave testing

--- examples/octave_test.py
+++ examples/octave_test.py
@@ -5,7 +5,7 @@ from pyknon.music import Note, NoteSeq
 
 filename = "scale_octaves.mid"
 notes1 = NoteSeq(
-    "C4' D E F G A B C4' B A G F E D C B, A G F E D C"
+    "C4 D E F G A B C4'' B' A G F E D C B, C' A G F E D C"
     )
 midi = Midi(1, tempo=90)
 midi.seq_notes(notes1, track=0)
