5e75d3c58a86568c6aef773efd5d00c48fd2e695
Evan Palmer <evjpalmer@gmail.com>
2015-10-06 14:30:13 -0700

    add other parts to marenzio.py

--- examples/marenzio.py
+++ examples/marenzio.py
@@ -2,12 +2,26 @@
 
 from __future__ import division
 from pyknon.genmidi import Midi
-from pyknon.music import Rest, Note, NoteSeq
+from pyknon.music import NoteSeq
 
 filename = "marenzio.mid"
-top_part = NoteSeq("A2 A A4 Bb2 Bb4 A2 A r4 D4''") + NoteSeq([Note(value=2,octave=6,dur=3/8)]) + NoteSeq("C8'' D4 Eb C2 Bb' D4'' C2 Bb4'") + NoteSeq([Note(value=9,octave=5,dur=3/8)]) + NoteSeq("Bb8 C2'' C r4 D") + NoteSeq([Note(value=2, octave=6, dur=3/4)]) + NoteSeq("C4'' Bb4' A G2 G") + NoteSeq("A4 A8 Bb C4'' Bb' A1 G1")
-middle_part = NoteSeq("")
-bottom_part = NoteSeq("")
-midi = Midi(number_tracks=3, tempo=120, instrument=0)
-midi.seq_notes(top_part)
+top_part = NoteSeq("""
+    A2 A A4 Bb2 Bb4 A2 A r4 D4'' D4. C8'' D4 Eb C2 Bb'
+    D4'' C2 Bb4' A4. Bb8 C2'' C r4 D D2. C4'' Bb4' A G2 G
+    A4 A8 Bb C4'' Bb' A1 G1.
+    """)
+middle_part = NoteSeq("""
+    F#2 F# F#4 G2 G4 F#2 F# r4 Bb4 Bb4. C8'' Bb4' Bb A2 Bb
+    Bb4 A2 G4 F4. G8 A2 A r4 Bb4 Bb2. A4 G F E2 E
+    F4 E8 D E4 G2 F#8 E F#2 G1.
+""")
+bottom_part = NoteSeq("""
+D2, D D4 G2,, G4 D2, D2 Bb,, Bb4., A8 G4 Eb4 F2 Bb,,
+Bb4,, F2, G4 D4. Eb8 F2 F G2 G2. F4 Eb D C2 C
+D4 C8 Bb, A4 G D1' G1.,
+""")
+midi = Midi(number_tracks=3, tempo=105, instrument=48)
+midi.seq_notes(top_part, track=0)
+midi.seq_notes(middle_part, track=1)
+midi.seq_notes(bottom_part, track=2)
 midi.write(filename)
