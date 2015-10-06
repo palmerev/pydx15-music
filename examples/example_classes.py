cf90fdd751f52e25f278ea271f78bb76c0db7729
Evan Palmer <evjpalmer@gmail.com>
2015-10-06 15:56:18 -0700

    condense example_classes.py

--- examples/example_classes.py
+++ examples/example_classes.py
@@ -1,6 +1,7 @@
 
-# Example representations of musical structures
-# NOT Pyknon code
+# Example representations only
+# NOT necessarily valid Pyknon code
+
 
 class RhythmSeq:
     '''A sequence of rhythmic durations'''
@@ -8,22 +9,12 @@ class RhythmSeq:
     pass
 
 # "r" represents a rest
-RhythmSeq("4 4 4 4") # four quarter notes
+RhythmSeq("4 4 4 4")  # four quarter notes
 RhythmSeq("4 4 8 8 4")
 
 RhythmSeq("4 8 8 4 4")
 RhythmSeq("r4 4 4 r4")
 
-
-
-
-
-
-
-
-###################################
-###################################
-###################################
 
 class NoteSeq:
     '''A sequence of notes with rhythms'''
@@ -38,30 +29,8 @@ NoteSeq("C4 r4 E4 D4 C2")
 NoteSeq("C4 r E D2 C") == NoteSeq("C4 r4 E4 D2 C2")
 
 
-
-
-
-
-
-
-####################################################
-####################################################
-####################################################
-
 # name/value + rhythm + octave
-NoteSeq("C2 C4, C C1'")
+NoteSeq("C4 C, C C' C''")
 
 # same octave and duration applied until changed
-NoteSeq("C#4' D8 E A4 C#'")
-
-
-
-
-
-
-
-
-
-###################################################
-###################################################
-###################################################
+NoteSeq("C#4'' D E A' C#''")
